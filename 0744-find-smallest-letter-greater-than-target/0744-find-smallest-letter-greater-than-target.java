ab82df4c6b33fe01644091b05b817490ac14500e
Vinay352 <vinay.j3097@gmail.com>
2023-10-17 04:26:16 -0400

    Time: 0 ms (100.00%), Space: 44 MB (74.72%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -15,6 +15,8 @@ class Solution {
 
 
         while(left <= right){
+            // exit the loop when left == right, basically when you have one element or the matching or "just less than" element w.r.t target
+
             int middle = left + (int)((right - left) / 2);
             if(letters[middle] <= target){
                 left = middle + 1;
@@ -25,6 +27,10 @@ class Solution {
         }
 
         return letters[right + 1];
+        // your aim is to output the smallest character that is greater than target
+        // at "right" index you have matching or "just less than" element w.r.t target
+        // therefore, (right + 1) index element will give you the immediately greater character than target
+
 
     }
 }
