292dc45b7d99b03b16c19eabaf45d4837fcacdde
Vinay Jain <vinay.j3097@gmail.com>
2023-11-07 11:01:52 -0500

    Time: 0 ms (100.00%), Space: 41.4 MB (25.06%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,29 +9,57 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    // ITERATIVE METHOD
+//     public ListNode reverseList(ListNode head) {
+//         if(head == null){
+//             return null;
+//         }
+//         if(head.next == null){
+//             return head;
+//         }
+//         ListNode temp = head;
+//         ListNode nextNode = temp.next;
+//         temp.next = null;
+//         ListNode nextNextNode = nextNode.next;
+
+//         while(nextNextNode != null){
+//             nextNode.next = temp;
+//             temp = nextNode;
+
+//             nextNode = nextNextNode;
+//             nextNextNode = nextNextNode.next;
+//         }
+
+//         nextNode.next = temp;
+//         temp = nextNode;
+
+//         return temp;
+//     }
+    
+    // RECURSION TYPE 1
+    public ListNode reverseList(ListNode head){
         if(head == null){
             return null;
         }
         if(head.next == null){
             return head;
         }
-        ListNode temp = head;
-        ListNode nextNode = temp.next;
-        temp.next = null;
-        ListNode nextNextNode = nextNode.next;
-
-        while(nextNextNode != null){
-            nextNode.next = temp;
-            temp = nextNode;
-
-            nextNode = nextNextNode;
-            nextNextNode = nextNextNode.next;
+        ListNode cur = head;
+        ListNode nextN = cur.next;
+        cur.next = null;
+        
+        return reverse_ll(cur, nextN, nextN.next);
+    }
+    // utility function
+    public ListNode reverse_ll(ListNode cur, ListNode nextN, ListNode nextNN){
+        if(nextN == null){
+            return cur;
         }
-
-        nextNode.next = temp;
-        temp = nextNode;
-
-        return temp;
+        
+        nextNN = nextN.next;
+        nextN.next = cur;
+        cur = nextN;
+        nextN = nextNN;
+        return reverse_ll(cur, nextN, nextNN);
     }
 }
