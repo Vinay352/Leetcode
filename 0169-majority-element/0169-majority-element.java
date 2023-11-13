4f7aa97102528d3553ed997d238700a9a715087d
Vinay Jain <vinay.j3097@gmail.com>
2023-11-13 16:27:19 -0500

    Time: 1 ms (99.91%), Space: 48.3 MB (57.37%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,7 +4,9 @@ class Solution {
         int count = 0; // count of the candidate which occurs moer than floor(n / 2) times
         int candidate = nums[0]; // the candidate with the max count as compared to others or count > floor(n / 2)
         for(int i = 0; i < nums.length; i++){
-            if(count == 0){ // if count is zero, assign the candidate to current array value
+            if(count == 0){ 
+                // if count is zero, assign the candidate to current array value
+                // that means there is a new candidate with a higher count
                 candidate = nums[i];
             }
 
