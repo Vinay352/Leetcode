22e8fde3994df08211ffd8f23ccce9af352e3141
Vinay Jain <vinay.j3097@gmail.com>
2023-11-13 16:25:54 -0500

    Time: 1 ms (99.91%), Space: 48.5 MB (30.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,22 @@
 class Solution {
+    // Moore's voting algorithm
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = nums[0];
+        int count = 0; // count of the candidate which occurs moer than floor(n / 2) times
+        int candidate = nums[0]; // the candidate with the max count as compared to others or count > floor(n / 2)
         for(int i = 0; i < nums.length; i++){
-            if(count == 0){
+            if(count == 0){ // if count is zero, assign the candidate to current array value
                 candidate = nums[i];
             }
 
-            if(candidate == nums[i]){
+            if(candidate == nums[i]){ // if nums[i] matches the candidate, increment the count
                 count++;
             }
-            else{
+            else{ // if it doesn't match, decrement count
                 count--;
             }
         }
-        // System.out.println(candidate);
+        // System.out.println(count);
+        
         return candidate;
     }
 }
