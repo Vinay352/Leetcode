db538987a9e9da6a4ef2aef95fe4c000c96be8c7
Vinay Jain <vinay.j3097@gmail.com>
2023-11-13 19:53:27 -0500

    Time: 9 ms (28.82%), Space: 42.1 MB (6.35%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public  List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<String>();
+        String path = "";
+
+        ans = binaryTreePathsUtil(root, path, ans);
+        for(int i = 0; i < ans.size(); i++){
+            String temp = ans.get(i);
+            ans.set(i, temp.substring(2));
+//            System.out.println( temp.substring(2) );
+        }
+
+        return ans;
+    }
+
+    public List<String> binaryTreePathsUtil(TreeNode root, String path, List<String> ans) {
+        if(root == null){
+            path = path + "";
+            return ans;
+        }
+
+        if(root.left == null && root.right == null){
+            path = path + "->" + root.val;
+            ans.add(path);
+            return ans;
+        }
+
+        path = path + "->" + root.val;
+
+        if(root.left != null){
+//            path = path + "->" + root.val;
+            ans = binaryTreePathsUtil(root.left, path, ans);
+        }
+
+        if(root.right != null){
+//            path = path + "->" + root.val;
+            ans = binaryTreePathsUtil(root.right, path, ans);
+        }
+
+        return ans;
+    }
+}
